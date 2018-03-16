db69de7c7249c5c7203ff59470fe35e64191df93
abhishek gupta <abhi19gupta@gmail.com>
2018-03-16 19:00:02 +0530

    Stream data collector

---
+++ data_collection/streaming.py
@@ -0,0 +1,57 @@
+try:
+    import json
+except ImportError:
+    import simplejson as json
+from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+import signal, sys
+from datetime import datetime
+
+f_log = open('log.txt','w')
+def log(s):
+    to_log = str(datetime.now()) + " | " + s
+    print(to_log)
+    f_log.write(to_log)
+    f_log.write('\n')
+
+ACCESS_TOKEN = '894945534410080256-ty8NTmEAUzzwJQTjSAfbmGp81HSVcZb'
+ACCESS_SECRET = 'plMGYeenmCZNs7gIDWrO17vEYFrm6GzgZ7BaJdPbMQYuL'
+CONSUMER_KEY = 'QC5nVHYoVYdNbl0oQeGExCmWW'
+CONSUMER_SECRET = 'e5EUAXqpYSjBZbhEpnqVCMd66WlSJTUJSCUdtQ5dIBmVlWYTIL'
+
+oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+twitter_stream = TwitterStream(auth=oauth)
+iterator = twitter_stream.statuses.sample()
+
+tweet_count = 1000
+num_read = 0
+f = open('stream_out.txt', 'w')
+f.write('[\n')
+
+def signal_handler(signal, frame):
+        log('You pressed Ctrl+C! Number of tweets read = %s'%str(num_read))
+        f.write('\n]')
+        f.close()
+        sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+for tweet in iterator:
+    # Twitter Python Tool wraps the data returned by Twitter as a TwitterDictResponse object.
+    # We convert it back to the JSON format to print/score
+    try:
+        x = 1/0
+        tweet_str = json.dumps(tweet, indent=4)
+        # print(tweet_str)
+        if (num_read != 0):
+            f.write(',\n')
+        f.write(tweet_str)
+        num_read+=1
+        if num_read == tweet_count:
+            break 
+        if num_read % 100 == 0:
+            log("Number of tweets read = %s"%str(num_read))
+    except Exception as e:
+        log("Exception occured: %s"%str(e))
+        continue
+
+f.write('\n]')
+f.close()
